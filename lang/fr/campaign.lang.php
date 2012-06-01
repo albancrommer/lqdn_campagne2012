75b1619f0bd74e63abcf3b6f5bc5f606ce01745b
Romain Gauthier <romain.gauthier@monkeypatch.me>
2012-06-01 23:30:16 +0200

    campaign.lang:  Delete trailing whitespaces.

--- lang/fr/campaign.lang.php
+++ lang/fr/campaign.lang.php
@@ -1,16 +1,16 @@
 <?php
 
 $translation = array(
- 
+
   "meta_title"      => "Help La Quadrature defends your rights online against ACTA and beyond ",
   "meta_desc"       => "
-        La Quadrature defends citizens rights and freedoms online since 2008. 
+        La Quadrature defends citizens rights and freedoms online since 2008.
         For the last 3 years, it helped raise awareness and campaign against ACTA.
         We must now finally defeat ACTA, and advocate for a positive agenda
         that will ensure that our freedoms will be guaranteed online.   ",
   "header_title"    => "La Quadrature du Net needs your help to continue its action. ",
   "header_desc"     => "
-        La Quadrature defends citizens rights and freedoms online since 2008. 
+        La Quadrature defends citizens rights and freedoms online since 2008.
         For the last 3 years, it helped raise awareness and campaign against ACTA.
         We must now finally defeat ACTA, and advocate for a positive agenda
         that will ensure that our freedoms will be guaranteed online.   ",
@@ -18,34 +18,36 @@ $translation = array(
   "header_more"     => "More about La Quadrature du Net",
   "acta_title"      => "Against ACTA...",
   "acta_points"     => array(
-      "More than 120 press releases, a webdossier, information campaign since 2008"          
-                    => "La Quadrature du Net documents and analyzes each and 
-                        every steps of ACTA since June 2008. Through leaking 
-                        drafts, alerting Members of the European Parliament and 
-                        publishing various analysis and press releases, La 
-                        Quadrature helped raised awareness among networked 
+      "More than 120 press releases, a webdossier, information campaign since 2008"
+                    => "La Quadrature du Net documents and analyzes each and
+                        every steps of ACTA since June 2008. Through leaking
+                        drafts, alerting Members of the European Parliament and
+                        publishing various analysis and press releases, La
+                        Quadrature helped raised awareness among networked
                         citizens and organizations. ",
-      "Citizen actions in the EU Parliament"       
-                    => "Around the completion of the Written Declaration 12, 
-                        where a majority of Members of the European Parliament 
+      "Citizen actions in the EU Parliament"
+                    => "Around the completion of the Written Declaration 12,
+                        where a majority of Members of the European Parliament
                         signed against ACTA, and during the various stages of
-                        the parliamentary process of ACTA, dozens of citizens 
-                        from all around Europe were flown in the European 
+                        the parliamentary process of ACTA, dozens of citizens
+                        from all around Europe were flown in the European
                         Parliament by La Quadrature's fundings and logisistics.",
-      "An animated movie seen by more than 2M persons."       
+      "An animated movie seen by more than 2M persons."
                     => "15 translations available",
-      "Upcoming steps to kill ACTA once and for all in early July"       
+      "Upcoming steps to kill ACTA once and for all in early July"
                     => ""
   ),
   "beyond_title"    => "...And Beyond!",
   "beyond_points"     => array(
-      "Our cultural practices (sharing, remix, etc) must become legal"          
+      "Our cultural practices (sharing, remix, etc) must become legal"
                     => "More ",
-      "The Internet must remain universal, net neutrality must be the rule"          
+      "The Internet must remain universal, net neutrality must be the rule"
                     => "On both landline and mobile networks... discriminations... Respectmynet... EU Level... ",
-      "( Free expression on the Net must be guaranteed and protected )"          
+      "( Free expression on the Net must be guaranteed and protected )"
                     => "More ",
-      "Building tools for empowering citizens to change things"          
+      "Building tools for empowering citizens to change things"
+                    => "More ",
+      "(2.5 full-time funded activists, a space in Paris)"
                     => "More "
   ),
   "panel_rollover"  => "⚠ Roll over or click on buttons to see details",
